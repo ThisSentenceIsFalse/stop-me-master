a9e2ea9af4e537a795b52fd1ffc348d47d546e39
Mihai Popescu <mihai-radu-niculae.popescu11@alumni.imperial.ac.uk>
2020-10-06 21:15:09 +0300

    Transition to hybrid: CJS default with /ESM

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,10 +1,20 @@
 const pkgMeta = require('./package.json');
 
-module.exports = {
-  input: './src/overlord.js',
-  output: {
-      file: pkgMeta.main,
-      format: 'es'
-  },
-  external: ['events', 'child_process']
-};
+module.exports = [
+    {
+        input: './src/overlord.js',
+        output: {
+            file: pkgMeta.module,
+            format: 'es'
+        },
+        external: ['events', 'child_process']
+    },
+    {
+        input: './src/overlord.js',
+        output: {
+            file: pkgMeta.main,
+            format: 'cjs'
+        },
+        external: ['events', 'child_process']
+    }
+];
